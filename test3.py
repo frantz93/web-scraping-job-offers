22648365981dafe1558c2ab8aca895c2483c8b5b
frantz93 <frantzroby@gmail.com>
2022-08-01 02:33:34 -0400

    added files and code

--- test3.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-
-root = Tk()
-#root.filename =  filedialog.askopenfilename(initialdir= "/", title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-root.directory = filedialog.askdirectory()
-print (root.directory)
